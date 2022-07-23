38f27479b8426d953512dd736489d7552d2cc09c
James <75079031+AnUnderratedPerson@users.noreply.github.com>
2022-07-23 17:15:18 +0000

    Removed unnecessary files

--- CMakeSource.cmake
+++
@@ -1,8 +0,0 @@
-target_sources(Genesis PUBLIC 
-    # AST COMPONENTS
-    "${Genesis.INCLUDE}/AST/Lexer.hpp"
-    "${Genesis.INCLUDE}/AST/TokenClass.hpp"
-
-    # OTHER COMPONENTS
-    "${Genesis.INCLUDE}/Util/Source.hpp"
-)
